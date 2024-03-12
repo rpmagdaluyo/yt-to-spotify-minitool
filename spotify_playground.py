848dc4f14578d15c6b663e7594b83ae63561741e
Richard <rpmagdaluyo@gmail.com>
2024-03-11 23:59:10 -0700

    Added TODOs

--- spotify_playground.py
+++ spotify_playground.py
@@ -7,13 +7,38 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+#TODO: Add YT-side functions
+""" YT Vars """
+yt_title = ''
+yt_description = ''
+yt_song_list = []
+
+""" YT Funcs """
+def get_description(yt_url):
+    return
+
+def get_title(yt_url):
+    return
+
+def get_song_list(yt_description):
+    return
+
+
+""" Spotipy Vars """
+
 spotify_client_id = "2f3d672957d34b17ba218fa9359c9936"
 spotify_client_secret = "8591f2c4217445fc9c372fcf52b3135a"
 spotify_redirect_uri = "http://localhost:3000"
-scopes = "playlist-modify-public, playlist-modify-private, user-library-read, ugc-image-upload"
+scopes = "playlist-modify-public, \
+          playlist-modify-private, \
+          user-library-read, \
+          ugc-image-upload"
+
 username = "richardarc18"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri=spotify_redirect_uri, scope=scopes))
+
+""" Spotify Funcs """
 
 def create_playlist(playlist_name):
     sp.user_playlist_create(user=username,
@@ -21,15 +46,22 @@ def create_playlist(playlist_name):
                             public=True,
                             description="")
 
-all_playlists = sp.user_playlists(user=username)
-newest_playlist_id = all_playlists["items"][0]["id"]
-
 #_authorization, _bearer = list(sp._auth_headers().items())[0]
 
 def update_playlist_cover(playlist_id, cover_image):
     sp.playlist_upload_cover_image(playlist_id=playlist_id, 
                                    image_b64=cover_image)
-    
+
+#TODO: Test Spotipy search and add songs
+def add_songs(playlist_id, song_list):
+    for song in song_list:
+        #search for the song
+        # add the song to the target playlist
+        return
+
+all_playlists = sp.user_playlists(user=username)
+newest_playlist_id = all_playlists["items"][0]["id"]
+
 #playlist_name = "(여자)아이들 ((G)I-DLE) Killing Voice (Vol. 2)"
 playlist_name = "TEST PLAYLIST"
 create_playlist(playlist_name)
@@ -41,10 +73,3 @@ with open(image_path, "rb") as image_file:
     new_playlist_cover_b64 = base64.b64encode(image_file.read()).decode("utf-8")
 
 update_playlist_cover(newest_playlist_id, new_playlist_cover_b64)
-
-"""
-TODO: Try refreshing the access token before using the playlist_upload_cover_image() method.
-"""
-#sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id, client_secret=spotify_client_secret, redirect_uri=spotify_redirect_uri, scope=scope))
-
-#print(sp.playlist_upload_cover_image(playlist_id="0DpU9vCfdvLNpeTCCJdRwI", image_b64=new_playlist_cover_b64))
